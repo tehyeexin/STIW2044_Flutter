5efc2d367ddb48df0715761f234f85fb70b26e37
Teh Yee Xin <73182761+tehyeexin@users.noreply.github.com>
2022-12-14 23:20:40 +0800

    Delete lab_asg_2_homestay_raya directory

--- lab_asg_2_homestay_raya/lab_asg_2_homestay_raya/lib/config.dart
+++
@@ -1,3 +0,0 @@
-class Config {
-  static const String SERVER = "http://10.31.216.241/homestayraya";
-}
--- lab_asg_2_homestay_raya/lib/config.dart
+++
@@ -1,3 +0,0 @@
-class Config {
-  static const String SERVER = "http://10.31.36.89/homestayraya";
-}
