ca53fce3c1f42baeaf3a9e99b1f3a75b29db4fe9
Teh Yee Xin <73182761+tehyeexin@users.noreply.github.com>
2022-12-17 12:37:38 +0800

    Add files via upload

---
+++ lab_asg_2_homestay_raya/homestay_raya_php/php/login_user.php
@@ -0,0 +1,39 @@
+<?php
+	if(!isset($_POST)){
+			$response = array('status' => 'failed', 'data' => null);
+			sendJsonResponse($response);
+			die();
+	}
+	
+	include_once("dbconnect.php");
+	
+	$email = $_POST['email'];
+	$password = sha1($_POST['password']);
+	$sqllogin = "SELECT * FROM tbl_users WHERE user_email = '$email' AND
+user_password = '$password'";
+	
+	$result = $conn->query($sqllogin);
+	
+	if($result->num_rows > 0){
+		while($row = $result->fetch_assoc()){
+			$userlist = array();
+			$userlist['id'] = $row['user_id'];
+			$userlist['name'] = $row['user_name'];
+			$userlist['email'] = $row['user_email'];
+			$userlist['phone'] = $row['user_phone'];
+			$userlist['regdate'] = $row['user_regdate'];
+			$response = array('status' => 'success', 'data' => $userlist);
+			sendJsonResponse($response);
+		}
+	} else{
+		$response = array('status' => 'failed', 'data' => null);
+			sendJsonResponse($response);
+	}
+	$conn->close();
+	
+	function sendJsonResponse($sentArray){
+		header('Content-Type: application/json');
+		echo json_encode($sentArray);
+	}
+
+?>
